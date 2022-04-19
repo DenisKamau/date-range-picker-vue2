58b16a425b7bff674263696c15c8d25a73674c38
Denis Kimani <denokim55@gmail.com>
2022-04-19 11:32:52 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <date-range-picker v-model="pickerDates">
+      <template>{{ pickerDates.startDate }} - {{ pickerDates.endDate }}</template>
+    </date-range-picker>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DateRangePicker from "vue2-daterange-picker";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    DateRangePicker,
+  },
+  data() {
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + 6);
+
+    return {
+      pickerDates: {
+        startDate,
+        endDate,
+      },
+    };
+  },
+  watch: {
+    pickerDates: {
+      handler(newVal) {
+        // Selected Range Values
+        console.log(newVal.startDate.toLocaleDateString());
+        console.log(newVal.endDate.toLocaleDateString());
+      },
+      deep: true,
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
